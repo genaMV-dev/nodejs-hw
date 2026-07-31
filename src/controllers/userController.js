fb2ca5be283decfb5c16a58bfe254d716084da84
Ign1s123 <genamisinskij@gmail.com>
2026-08-01 02:46:15 +0300

    #

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -9,7 +9,7 @@ export const updateUserAvatar = async (req, res) => {
 
   const result = await saveFileToCloudinary(req.file.buffer, req.user._id);
 
-  const user = User.findOneAndUpdate(
+  const user = await User.findOneAndUpdate(
     { _id: req.user._id },
     { avatar: result.secure_url },
     { returnDocument: 'after' }
